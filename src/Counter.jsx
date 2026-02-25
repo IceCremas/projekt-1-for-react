2ca0c0d936d94cbfb2514c2756f470415f64fd07
3azil <145561424+3azil@users.noreply.github.com>
2026-02-25 14:21:13 +0300

    awd

---
+++ src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter(){
+    const [x, setX] = useState(0);
+  
+    const increment = () => {
+        setX(x - 1);
+    }; 
+    return (
+        <>
+            <h2>{x}</h2>
+            <button onClick={increment}>-</button>
+        </>
+    );
+}
